9f216558c7379696b01dd49f1d3a5553b1b52f70
theav21 <theamanverma21@gmail.com>
2022-02-27 12:15:01 +0530

    completed one project to make letter uppercase and lowercase

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React , { useState } from 'react'
 
-export default function (props) {
-  return (
-    <div className='container my-3'>
-        <h1>{props.heading}</h1>
-        <div className="mb-3">
-            <textarea className="form-control" id="exampleFormControlTextarea1" rows="15"></textarea>
+function TextForm (props) {
+      const clickHandler = () => {
+          console.log("button has been clicked");
+          let newText=text.toUpperCase();
+          setText(newText)
+      }
+      const clickHandler2 = () => {
+        console.log("button has been clicked");
+        let newText2=text.toLowerCase();
+        setText(newText2)
+    }
+
+
+      const changeHandler=(event)=>{
+        console.log("change has been made");
+        setText(event.target.value)
+      }
+
+      const [text, setText] = useState("");
+
+      return (
+        <div className='container my-3'>
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+                <textarea className="form-control" value={text} onChange={changeHandler} id="exampleFormControlTextarea1" rows="15" placeholder='enter text here'></textarea>
+            </div>
+            <button type='button' onClick={clickHandler}  className="btn btn-primary m-3">Convert to Uppercase</button>
+            <button type='button' onClick={clickHandler2}  className="btn btn-primary m-3">Convert to lowercase</button>
         </div>
-        <button type='button' className="btn btn-primary m-3">Convert to Uppercase</button>
-        {/* <button type='button' className="btn btn-primary m-3">Convert to Uppercase</button> */}
-    </div>
-  )
+      )
 }
+export default TextForm;
