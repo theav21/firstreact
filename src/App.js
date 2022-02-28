2a5b5956b90d8e30b4b610afb333845783fcb48e
theav21 <theamanverma21@gmail.com>
2022-02-28 23:09:42 +0530

    introduced the dark mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React  from 'react';
 
 function App() {
+  const [mode, setMode] = useState("light")
+  const [label , setLabel] = useState("dark")
+
+  const toggler=()=>{
+    if(mode==="light"){
+      setLabel("white")
+      setMode("dark")
+      document.body.style.backgroundColor ='black'
+    }
+    else{
+      setLabel("black")
+      setMode("light")
+      document.body.style.backgroundColor ='white'
+    }
+  }
+
+
+
   return (
     <>
-      <Navbar title="My-App"/>
-      <TextForm heading="Enter the Text to Analyze"/>
+      <Navbar title="My-App" mode={mode} toggle={toggler} label={label}/>
+      <TextForm  mode={mode} label={label}/>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -41,18 +41,18 @@ function TextForm (props) {
 
       return (
         <>
-        <div className="container my-3">
+        <div className="container my-3" style={{color:props.label}}>
           <h1>Words and Characters Count</h1>
           <p>{text.split(" ").length} words and {text.length} Characters</p>
           <p><b>Time to read below para :-</b>{0.008 * text.split(" ").length} minutes or {0.48 * text.split(" ").length} seconds </p>
-          <div class="wrapper1 accordion-item">
-                <h2 class="accordion-header" id="headingOne">
-                  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          <div className={`wrapper1 accordion-item bg-${props.mode}`}>
+                <h2 className="accordion-header" style={{color:props.label}} id="headingOne">
+                  <button className={`accordion-button bg-${props.mode}`} style={{color:props.label}} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                     <b>Find And Replace Word</b>
                   </button>
                 </h2>
-                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                  <div class="accordion-body">
+                <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                  <div className="accordion-body">
                       <div className="input-group mb-1">
                         <input type="text" className="form-control" value={fWord} onChange={handlefindChange} aria-describedby="button-addon2"/>
                         <input type="text" className="form-control" value={rWord} onChange={handlereplaceChange}  aria-describedby="button-addon2"/>
@@ -63,13 +63,13 @@ function TextForm (props) {
             </div>
 
         </div>
-        <div className='container my-3'>
-            <h1>{props.heading}</h1>
+        <div className='container my-3' style={{color:props.label}}>
+            <h1>Enter the Text to Analyze</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={changeHandler} id="exampleFormControlTextarea1" rows="10" placeholder='enter text area'></textarea>
+                <textarea className={`form-control bg-${props.mode}`} value={text} style={{color:props.label}}  onChange={changeHandler} id="exampleFormControlTextarea1" rows="10" placeholder='enter text area'></textarea>
             </div>
-            <button type='button' onClick={clickHandler}  className="btn btn-primary m-2">Convert to Uppercase</button>
-            <button type='button' onClick={clickHandler2}  className="btn btn-danger m-2">Convert to lowercase</button>
+            <button type='button' onClick={clickHandler} className={`btn btn-${props.mode} m-2`}>Convert to Uppercase</button>
+            <button type='button' onClick={clickHandler2}  className={`btn btn-${props.mode} m-2`}>Convert to lowercase</button>
             <button type='button' onClick={click}  className="btn btn-light m-2">Clear</button>
             <h2>Preview</h2>
             <p>{text}</p>
